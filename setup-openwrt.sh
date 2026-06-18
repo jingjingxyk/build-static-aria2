2f0b3e95678f676d7c807e682e7e5c4f0f5c1145
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-18 21:01:55 +0800

    update setup-openwrt.sh

--- setup-openwrt.sh
+++ setup-openwrt.sh
@@ -9,6 +9,27 @@ __PROJECT__=${__DIR__}
 shopt -s expand_aliases
 cd ${__PROJECT__}
 
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+  --proxy)
+    export HTTP_PROXY="$2"
+    export HTTPS_PROXY="$2"
+    NO_PROXY="127.0.0.0/8,10.0.0.0/8,100.64.0.0/10,172.16.0.0/12,192.168.0.0/16"
+    NO_PROXY="${NO_PROXY},::1/128,fe80::/10,fd00::/8,ff00::/8"
+    NO_PROXY="${NO_PROXY},localhost"
+    NO_PROXY="${NO_PROXY},.aliyuncs.com,.aliyun.com,.tencent.com"
+    NO_PROXY="${NO_PROXY},.myqcloud.com,.swoole.com"
+    export NO_PROXY="${NO_PROXY},.tsinghua.edu.cn,.ustc.edu.cn,.npmmirror.com"
+    ;;
+  --*)
+    echo "Illegal option $1"
+    ;;
+  esac
+  shift $(($# > 0 ? 1 : 0))
+done
+
+
 mkdir -p ${__PROJECT__}/var/
 cd ${__PROJECT__}/var/
 
@@ -30,7 +51,7 @@ cd ${APP_NAME}
 # 参考文档
 # https://openwrt.org/docs/guide-user/virtualization/virtualbox-vm
 
-# curl -LSo openwrt-25.12.3-x86-generic-generic-ext4-combined.img.gz https://archive.openwrt.org/releases/25.12.3/targets/x86/generic/openwrt-25.12.3-x86-generic-generic-ext4-combined.img.gz
+# curl -LSo openwrt-25.12.3-x86-64-generic-ext4-combined.img.gz https://archive.openwrt.org/releases/25.12.3/targets/x86/64/openwrt-25.12.3-x86-64-generic-ext4-combined.img.gz
 
 if [ -f runtime/aria2c/aria2c ]; then
   curl -fSL https://gitee.com/jingjingxyk/swoole-cli/raw/new_dev/setup-aria2-runtime.sh?raw=ture | bash -s -- --mirror china
@@ -43,23 +64,21 @@ aria2c --file-allocation=none -c -x4 -s16 \
   https://mirrors.cqupt.edu.cn/openwrt/releases/${VERSION}/targets/x86/64/openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img.gz \
   https://mirrors.hit.edu.cn/openwrt/releases/${VERSION}/targets/x86/64/openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img.gz
 
-# https://archive.openwrt.org/releases/25.12.3/targets/x86/64/openwrt-25.12.3-x86-64-generic-ext4-combined.img.gz
 
 test -f openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img && rm -f openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img
-# gzip -d openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img.gz
-# 保持解压前原文件
-gzip -c openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img.gz >openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img
+gzip -d openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img.gz
 
 # 将文件填充到对其
 test -f openwrt.img && rm -f openwrt.img
 dd if=openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img of=openwrt.img bs=128000 conv=sync
+# truncate -s %512 openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img
 
 # img 文件转换为 vdi
 test -f openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi && rm -f openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi
-VBoxManage convertdd openwrt.img openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi
+VBoxManage convertfromraw --format VDI openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.img openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi
 
 # 显示vdi 信息
 VBoxManage showhdinfo openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi
 
 # 修改磁盘大小 8GB
-VBoxManage modifymedium openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi --resize 8096
+VBoxManage modifymedium disk openwrt-${APP_VERSION}-x86-64-generic-ext4-combined.vdi --resize 8096
