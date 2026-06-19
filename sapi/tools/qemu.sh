1ea012c20730035a2d2c6216c0c6fd8588b1db5b
jingjingxyk <zonghengbaihe521@qq.com>
2026-06-19 09:11:12 +0800

    update

--- sapi/tools/qemu.sh
+++ sapi/tools/qemu.sh
@@ -1,12 +1,9 @@
-
-export LOGICAL_PROCESSORS=$(nproc 2> /dev/null || sysctl -n hw.ncpu)
+export LOGICAL_PROCESSORS=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
 export CMAKE_BUILD_PARALLEL_LEVEL=${LOGICAL_PROCESSORS}
 
-
-
-test -f qemu-9.2.0.tar.xz ||  curl -fSLo qemu-9.2.0.tar.xz https://download.qemu.org/qemu-9.2.0.tar.xz
-test -d  qemu-9.2.0 && rm -rf qemu-9.2.0
-tar xvJf qemu-9.2.0.tar.xz
-cd qemu-9.2.0
+test -f qemu-11.0.1.tar.xz || curl -fSLo qemu-11.0.1.tar.xz https://download.qemu.org/qemu-11.0.1.tar.xz
+test -d qemu-11.0.1 && rm -rf qemu-11.0.1
+tar xvJf qemu-11.0.1.tar.xz
+cd qemu-11.0.1
 ./configure
 make -j ${LOGICAL_PROCESSORS}
